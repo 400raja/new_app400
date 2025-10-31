02ae68ca7fe05b8c9c1e249ff9a2f36893a721d0
raja <rashok-adm@transportforlondon.onmicrosoft.com>
2025-10-31 12:24:25 +0000

    changes in A

--- new_app400/Program.cs
+++ new_app400/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! tamil made this change GIT CHG");
+app.MapGet("/", () => "Hello World! tamil made this change GIT CHG A");
 
 app.Run();
